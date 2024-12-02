c8ac2185f05750f58a0f6ca6252b2d1442075044
soldi <rodrigosoldi@icloud.com>
2024-12-02 08:55:38 -0300

    async update

--- Sources/RSSwiftUtils/RSAsync.swift
+++ Sources/RSSwiftUtils/RSAsync.swift
@@ -7,11 +7,23 @@
 
 import Foundation
 
+/// Executes an asynchronous task with a specified timeout. If the task does not complete
+/// within the given timeout, a fallback operation is executed.
+/// - Parameters:
+///   - timeout: The maximum time in seconds to wait for the operation to complete.
+///   - operation: The asynchronous operation to execute.
+///   - fallback: The asynchronous fallback operation to execute if the timeout is reached.
+/// - Returns: The result of the operation if it completes in time, otherwise the result of the fallback.
+/// - Throws: An error if the operation or fallback throws an error.
 public func timeoutAsyncTask<T>(timeout: Int, _ operation: @escaping () async throws -> T, fallback: @escaping () async throws -> T) async throws -> T {
     let mainTask = Task {
-        let taskResult: T = try await operation()
-        try Task.checkCancellation()
-        return taskResult
+        do {
+            let taskResult: T = try await operation()
+            try Task.checkCancellation()
+            return taskResult
+        } catch is CancellationError {
+            throw CancellationError()
+        }
     }
         
     let timeoutTask = Task {
@@ -20,8 +32,11 @@ public func timeoutAsyncTask<T>(timeout: Int, _ operation: @escaping () async th
         return try await fallback()
     }
         
-    let result = try await mainTask.value
-    timeoutTask.cancel()
-    return result
+    do {
+        let result = try await mainTask.value
+        timeoutTask.cancel()
+        return result
+    } catch is CancellationError {
+        return try await timeoutTask.value
+    }
 }
-
